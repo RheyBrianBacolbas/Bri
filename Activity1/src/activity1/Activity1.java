d62def9c7eedc09c5d1b6d7aaad1710378be79c2
Student <Student@CLB3Unit03>
2024-02-13 14:45:15 +0800

    Version 1.0 dog

---
+++ Activity1/src/activity1/Activity1.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1;
+
+/**
+ *
+ * @author Student
+ */
+public class Activity1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic /*
+
+
+
+///**
+
+import java.util.Scanner;
+
+// *
+// * @author Student
+// */
+//public class Activity {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) {
+//    
+//        String firstName = "Christian Hanz Balot";
+//        String middleName = "Gonzaga";
+//        String lastName = "Cantomayor";
+//        int age = 4500;
+//        char gender = 'B';
+//        boolean isHuman = true;
+//        
+//        System.out.println("First Name:" + firstName);
+//        System.out.println("Middle Name:" + middleName);
+//        System.out.println("Last Name:" + lastName);
+//        System.out.println("Gender:" + gender);
+//        System.out.println("Age:" + age);
+//        System.out.println("Gusto nimo si manang?:" + isHuman);
+//      
+//        
+//        
+//        
+       
+
+        
+//    public class Activity {
+//
+//    /**
+//     * @param args the command line arguments
+//     */
+//    public static void main(String[] args) { 
+//        
+//
+//        
+//        Scanner scn;
+//        scn = new Scanner(System.in);
+//        
+//        System.out.println("Enter the value of num1");
+//        String num1 = scn.nextLine();
+//        
+//        System.out.println("Enter the value of num2");
+//        String num2 = scn.nextLine();
+//        
+//        System.out.println("Value of num1:" + num1 + " " +"Value of num2:" + num2);
+//        
+        
+        
+        
+                
+    
+ }
+    
+}
+
+    }
+    
+}
